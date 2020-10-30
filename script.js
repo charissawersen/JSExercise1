ca5247cd587f42e1b58a1a11ab5a4ead04f50765
Charissa Wersen <charissa.wersen@gmail.com>
2020-10-30 10:39:29 -0400

    JS exercise 1

---
+++ script.js
@@ -0,0 +1,24 @@
+//var msg = 'Hello World'; 
+//console.log(msg);
+
+const actualTemp = 68;
+const desiredTemp = 65;
+
+//tell heating and cooling system to run a/c
+//if (actualTemp > desiredTemp) {
+//print Run A/C
+//console.log('Run A/C');}
+
+//tell heating and cooling system to run heat
+//if (desiredTemp < 70) {
+   // console.log("Run heat");}
+//print Run Heat
+
+//tell heating & cooling system to stand-by
+//if (actualTemp <= 70 && desiredTemp <=67 ) {
+  //  console.log("Standyby");}
+
+if (actualTemp < desiredTemp) {
+    console.log("Run heat");}
+    else{console.log("nothing");}
+    
